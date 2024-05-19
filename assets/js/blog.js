96d0bf93bc665e16504b93f71b5a22aab28568a2
anrichter <anrichter2@gmail.com>
2024-05-18 22:24:18 -0500

    Filled out the blog HTML and CSS files to create the basic layout for the blog site. Also filled out the blog javaScript file to add light and dark mode functionality for the blog site.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,18 @@
-//Todo: Take blog info from local storage, use .createElement to dynamically create elements with user information, Append information into blog
+//Todo: Take blog info from local storage, use .createElement to dynamically create elements with user information, use .appendchild to get information into blog
+//Also add light/dark mode controls
+
+const modeToggle = document.querySelector('#mode-toggle');
+const container = document.querySelector('.container')
+
+let mode = 'light';
+
+modeToggle.addEventListener('click', function() {
+    if (mode === 'light') {
+        mode = 'dark'
+        container.setAttribute('class', 'dark');
+    } else {
+        mode = 'light'
+        container.setAttribute('class', 'light')
+    }
+})
+
