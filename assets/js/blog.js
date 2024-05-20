31582ca07cf78bc7118a9c20bd98a48da5fcff65
anrichter <anrichter2@gmail.com>
2024-05-19 22:13:59 -0500

    Added anchor tags to the buttons in index.html and blog.html so that you can switch between the two pages when clicked. One of the buttons works, the other doesn't. Filled out form.js so now there is code for the light/dark mode toggle that hasn't been fully intergrated yet. Also added code for an eventlistner for my form button that push methods the forms information into an array that then gets saved to local storage, and added alerts that tell the user to completely fill out the form, if they omitted a part, and ends the funtion before the information is saved in that case. Added functions in blog.js that will take the form information from local storage and save it as an array and then uses the forms information to create new elements that get addd to the blog.html page.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,16 +3,47 @@
 
 const modeToggle = document.querySelector('#mode-toggle');
 const container = document.querySelector('.container')
+const mainEl = document.querySelector('.blog-posts')
+
+const blogPosts = [];
 
 let mode = 'light';
 
 modeToggle.addEventListener('click', function() {
     if (mode === 'light') {
-        mode = 'dark'
+        mode = 'dark';
         container.setAttribute('class', 'dark');
     } else {
-        mode = 'light'
-        container.setAttribute('class', 'light')
+        mode = 'light';
+        container.setAttribute('class', 'light');
     }
-})
+});
 
+function renderPosts() {
+    for (let i = 0; i < blogPosts.length; i++) {
+        const blogPosts = blogPosts[i];
+
+        const div = document.createElement('div');
+        const title = document.createElement('h3');
+        const content = document.createElement('p');
+        const username = document.createElement('h4');
+        h3.textContent = blogPosts[i].title;
+        p.textContent = blogPosts[i].content;
+        h4.textContent = blogPosts[i].username;
+        mainEl.appendChild(div);
+        div.appendChild(title);
+        title.appendChild(content);
+        content.appendChild(username);
+    }
+}
+
+function init() {
+    const formInfo = JSON.parse(localStorage.getItem('formInfo'));
+    if (formInfo !== null) {
+        blogPosts = formInfo;
+    }
+    renderPosts()
+};
+
+
+init()
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,41 @@
 //TODO: Collect and store blog information and save to local storage
+//activity 23
+
+const modeToggle = document.querySelector('#mode-toggle');
+const username = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitButton = document.getElementById('submit');
+
+let mode = 'light';
+
+modeToggle.addEventListener('click', function() {
+    if (mode === 'light') {
+        mode = 'dark';
+        container.setAttribute('class', 'dark');
+    } else {
+        mode = 'light';
+        container.setAttribute('class', 'light');
+    }
+});
+
+const formInfo = []
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (username.value === '') {
+        alert('Please complete the entire form');
+        return;
+    } else if (title.value === '') {
+        alert('Please complete the entire form');
+        return;
+    } else if (content.value === '') {
+        alert('Please complete the entire form');
+        return;
+    }
+
+    formInfo.push({username: username.value, title: title.value, content: content.value.trim()});
+
+    localStorage.setItem('formInfo', JSON.stringify(formInfo));
+});
